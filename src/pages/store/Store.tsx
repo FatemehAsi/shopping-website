e88afa2cd11f0a17fd4ea248c8c4511d51a34317
Fatemeh Asi <fatemeh.asi.2000@gmail.com>
2025-07-10 23:17:13 +0330

    [GET] Products API

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,19 +1,27 @@
 import ProductItem from "../../components/productItem/ProductItem.tsx";
 import Container from "../../components/container/Container";
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {getProducts} from "../../services/api.tsx";
 
 function Store() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+            getProducts().then((res) => {setProducts(res)})
+        }
+        ,[])
+
     return(
         <div>
             <Container>
             <h1 className="text-right mt-5">جدیدترین محصولات</h1>
             <div className='grid grid-cols-4 gap-4'>
-
-             <Link to='product/${1}'><ProductItem /></Link>
-             <Link to='product/${2}'><ProductItem /></Link>
-             <Link to='product/${3}'><ProductItem /></Link>
-             <Link to='product/${4}'><ProductItem /></Link>
-             <Link to='product/${5}'><ProductItem /></Link>
+                {products.map(product => (
+                    <Link to={`/products/${1}`}>
+                        <ProductItem/>
+                    </Link>
+                ))}
 
             </div>
             </Container>
